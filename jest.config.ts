4262a378c1d90308a6bf1f23591c160dc5329b4f
donguks <donguk.seo@modori.io>
2025-03-05 17:07:03 +0900

    chore(jest): increase test timeout to 20 seconds

--- jest.config.ts
+++ jest.config.ts
@@ -22,6 +22,7 @@ const config: Config = {
     projects,
     verbose: true,
     coverageDirectory: "./coverage/libraries",
+    testTimeout: 20000,
     coverageThreshold: {
         global: {
             branches: 90,
