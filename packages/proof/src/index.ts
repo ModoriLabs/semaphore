760517d5eeda3606fd24556b79b8abf2c50e3d74
kyngwan@gmail.com <kyngwan@gmail.com>
2025-03-07 17:58:57 +0900

    feat(proof): implement generate inputs 2

--- packages/proof/src/index.ts
+++ packages/proof/src/index.ts
@@ -1,6 +1,7 @@
 import { packGroth16Proof, unpackGroth16Proof } from "@zk-kit/utils/proof-packing"
 import generateProof from "./generate-proof"
 import verifyProof from "./verify-proof"
+import generateInputs from "./generate-inputs"
 
 export * from "./types"
-export { generateProof, packGroth16Proof, unpackGroth16Proof, verifyProof }
+export { generateProof, packGroth16Proof, unpackGroth16Proof, verifyProof, generateInputs }
